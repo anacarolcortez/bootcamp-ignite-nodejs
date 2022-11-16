68c88459d269e5d8b7a31fb62dac3f26ff466540
anacarolcortez <anacarolcortez@hotmail.com>
2022-11-16 15:23:33 -0300

    aula2 delete dist

--- ts-rentacar/dist/CreateCourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCourseService {
-    execute({ name, duration, educator }) {
-        console.log(`${name}, ${duration} weeks, ${educator}`);
-    }
-}
-exports.default = new CreateCourseService();
