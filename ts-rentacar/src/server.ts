67a37e3c69d11b7d26729af40f7e3f331b787cca
anacarolcortez <anacarolcortez@hotmail.com>
2022-11-19 10:08:49 -0300

    aula3 typeorm e docker configs

--- ts-rentacar/src/server.ts
+++ ts-rentacar/src/server.ts
@@ -2,10 +2,13 @@ import express from "express"
 import { router } from "./routes"
 import swaggerUi from "swagger-ui-express"
 import swaggerFile from "./swagger.json"
+import "./database"
 
 const app = express()
+
 app.use(express.json())
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(router)
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
